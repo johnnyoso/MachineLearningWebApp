c1835caf18e2cad463bf34291dc358a97790a8f4
John Osorio <johnpatrick.osorio@yahoo.com>
2022-10-18 23:08:26 +1100

    Updates

--- deploy-mlmodel-project/app.py
+++ deploy-mlmodel-project/app.py
@@ -4,21 +4,37 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open(r'C:\Users\johnp\deploy-mlmodel\MachineLearningWebApp\deploy-mlmodel-project\og_log_reg.pkl', 'rb'))
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    
+    teams = {'1838315' : 'Team Secret', 
+             '2586976' : 'OG', 
+             '2163'    : 'Team Liquid', 
+             '7732977' : 'Boom Esports', 
+             '15'      : 'PSG.LGD', 
+             '7119388' : 'Team Spirit', 
+             '8254400' : 'Beastcoast', 
+             '6209166' : 'Team Aster', 
+             '7391077' : 'Thunder Awaken', 
+             '8260983' : 'TSM', 
+             '8291895' : 'Tundra Esports', 
+             '7390454' : 'Soniqs', 
+             '8599101' : 'Gaimin Gladiators', 
+             '39'      : 'Evil Geniuses', 
+             '350190'  : 'Fnatic', 
+             '8131728' : 'Hokori', 
+             '8605863' : 'Entity', 
+             '8721219' : 'BetBoom Team', 
+             '6209804' : 'Royal Never Give Up', 
+             '8597976' : 'Talon Esports'} 
+    
+    predict_team_id = request.form.get('predict_team_names')
+    opposing_team_id = request.form.get('opposing_team_names')
 
-@app.route('/predict', methods = ['GET', 'POST'])
-def predict():
+    print('predict_team_id', predict_team_id)
+    print('opposing_team_id', opposing_team_id)
 
-    predict_team = request.form.get('predict-team-names')
-    opposing_team = request.form.get('opposing-team-names')
-
-    print(predict_team)
-    print(opposing_team)
-
-    return render_template('index.html')
-
+    return render_template('index.html', teams = teams)
 
 if __name__ == "__main__":
     app.run()
